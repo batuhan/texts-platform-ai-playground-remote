d1a9052e3ddbaf30b9bfafc821698044dc48b712
Alper Değre <alper.degre.dev@gmail.com>
2024-02-05 21:09:18 +0300

    rename and icons

--- src/info.ts
+++ src/info.ts
@@ -3,14 +3,13 @@ import {
   MessageDeletionMode,
   Attribute,
 } from "@textshq/platform-sdk";
+import { PLATFORM_ICON } from "./icons";
 
 const info: PlatformInfo = {
-  name: "platform-remote",
+  name: "platform-ai-playground-rest",
   version: "1.0.0",
-  displayName: "Platform Remote",
-  icon: `<svg width="1em" height="1em" viewBox="0 0 1000 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 500C0 223.858 223.858 0 500 0C776.142 0 1000 223.858 1000 500C1000 776.142 776.142 1000 500 1000H62.5C27.9822 1000 0 972.017 0 937.5V500Z" fill="currentColor" />
-</svg>`,
+  displayName: "platform-ai-playground-rest",
+  icon: PLATFORM_ICON,
   loginMode: "custom",
   deletionMode: MessageDeletionMode.UNSUPPORTED,
   attributes: new Set([
